5f97ab89cb773a12b17da03eb1fe18d6468f1282
Michele Iacobone <michele.iacobone@gmail.com>
2015-01-20 12:07:25 +0100

    look at battery_model.m

--- battery_model.m
+++ battery_model.m
@@ -4,28 +4,28 @@ lower_current=1.440;
 upper=load('upper_curve.txt');
 lower=load('lower_curve.txt');
 
-#Contains a cap-voc table by row
-#cap vcc
-#cap2 vcc2
-#cap3 vcc3
+%Contains a cap-voc table by row
+%cap vcc
+%cap2 vcc2
+%cap3 vcc3
 x_interp_upper= 0:0.1:max(upper(:,+1));
 upper_interp=[x_interp_upper' interp1(upper(:,1), upper(:,2), x_interp_upper)'];
 
 x_interp_lower= 0:0.1:max(lower(:,+1));
 lower_interp=[x_interp_lower' interp1(lower(:,1), lower(:,2), x_interp_lower)'];
 
-#Obtain data in a different number of points
+%Obtain data in a different number of points
 max_interp_value=min(max(upper(:,1)), max(lower(:,1)));
 min_interp_value=max(min(upper(:,1)), min(lower(:,1)));
 
-#Taglia il vettore
+%Taglia il vettore
 upper_interp=upper_interp(find( upper_interp(:,1)>min_interp_value), :);
 upper_interp=upper_interp(find( upper_interp(:,1)<max_interp_value), :);
 
 lower_interp=lower_interp(find( lower_interp(:,1)>min_interp_value), :);
 lower_interp=lower_interp(find( lower_interp(:,1)<max_interp_value), :);
 
-#Normalize the vector
+%Normalize the vector
 divide_by=max_interp_value-min_interp_value;
 upper_interp(:,1)=upper_interp(:,1)/divide_by;
 lower_interp(:,1)=lower_interp(:,1)/divide_by;
@@ -46,15 +46,16 @@ for i=1:length(upper_interp(:,1))
   R=(Vbc2-Vbc1)/(Ibc1-Ibc2);
   Voc=Vbc1+R*Ibc1;
 
-  #Build a Cap-R-V table
+  %Build a Cap-R-V table
   Rs=[Rs;lower_interp(i,1) R Voc];
 end
 
 
-#Interpolate equation for VSoc
+%Interpolate equation for VSoc
 x=[min(Rs(:,1)):0.001:max(Rs(:,1))];
 p0=[1 1 1 1 1 1 1];
-p=fmins('vsoc_error', p0, [], [], Rs(:,1), Rs(:,3));
+p=fminsearch('vsoc_error', p0, [], [], Rs(:,1), Rs(:,3));
+p1=p;
 x=[min(Rs(:,1)):0.001:max(Rs(:,1))];
 y=feval('vsoc2voc', p, x);
 figure;
@@ -63,10 +64,11 @@ title('Measured vs. fitted VSOC curve', 'Fontname', 'DejaVu', 'FontSize', 16);
 xlabel('Normalized charge', 'FontName', 'DejaVu', 'FontSize', 12);
 ylabel('VSOC', 'FontName', 'DejaVu', 'FontSize', 12);
 
-#Interpolate equation for R
+%Interpolate equation for R
 x=[min(Rs(:,1)):0.001:max(Rs(:,1))];
 p0=[1 1 1];
 p=fmins('r_error', p0, [], [], Rs(:,1), Rs(:,2));
+p2=p;
 x=[min(Rs(:,1)):0.001:max(Rs(:,1))];
 y=feval('vsoc2r', p, x);
 figure;
